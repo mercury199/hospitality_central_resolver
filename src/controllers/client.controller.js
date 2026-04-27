39d4a8b516fc3a24c9552c558366089b831ec514
Srijan Bhattacharjee <srijan@capitalnumbers.com>
2026-04-27 21:55:50 +0530

    api for testing colors

--- src/controllers/client.controller.js
+++ src/controllers/client.controller.js
@@ -1,4 +1,8 @@
-const { getAppStylingDetail, createClientConfig } = require('../services/client.service');
+const {
+  getAppStylingDetail,
+  createClientConfig,
+  getAppStylingDetailV2,
+} = require('../services/client.service');
 
 const fetchAppStylingDetail = async (req, res) => {
   try {
@@ -59,7 +63,31 @@ const addClientConfig = async (req, res) => {
   }
 };
 
+const fetchAppStylingDetailV2 = async (req, res) => {
+  try {
+    const { clientCode } = req.query;
+
+    if (!clientCode) {
+      return res.status(400).json({
+        message: 'clientId query param is required',
+      });
+    }
+
+    const data = await getAppStylingDetailV2(clientCode);
+    return res.status(200).json(data);
+  } catch (error) {
+    const statusCode = error.response?.status || 500;
+    const details = error.response?.data || error.message;
+
+    return res.status(statusCode).json({
+      message: 'Failed to fetch app styling detail from Strapi',
+      details,
+    });
+  }
+};
+
 module.exports = {
   fetchAppStylingDetail,
   addClientConfig,
+  fetchAppStylingDetailV2,
 };
